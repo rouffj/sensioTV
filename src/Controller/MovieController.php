bed72ab5e5a5ff2b472c402420cd812cbe727286
Joseph Rouff <rouffj@gmail.com>
2021-05-21 00:56:58 +0200

    exo3: Add fixtures

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -17,7 +17,7 @@ class MovieController extends AbstractController
     public function index(): Response
     {
         return $this->render('movie/home.html.twig', [
-            'controller_name' => 'MovieController',
+            'movies' => $this->getMovies(),
         ]);
     }
 
@@ -73,5 +73,17 @@ class MovieController extends AbstractController
     public function name($name): Response
     {
         return new Response(sprintf('Hello %s', $name), 201);
+    }
+
+    private function getMovies()
+    {
+        return [
+            ['id' => 1, 'title' => 'Memento', 'releaseDate' => 2000, 'director' => 'Christopher Nolan', 'duration' => '113', 'image' => '/assets/images/movie-image-samples/memento.jpeg'],
+            ['id' => 2, 'title' => 'Insomnia', 'releaseDate' => 2002, 'director' => 'Christopher Nolan', 'duration' => '118', 'image' => '/assets/images/movie-image-samples/insomnia.jpeg'],
+            ['id' => 3, 'title' => 'The Dark Knight ', 'releaseDate' => 2008, 'director' => 'Christopher Nolan', 'duration' => '152', 'image' => '/assets/images/movie-image-samples/the-dark-knight.jpeg'],
+            ['id' => 4, 'title' => 'Inception', 'releaseDate' => 2010, 'director' => 'Christopher Nolan', 'duration' => '148', 'image' => '/assets/images/movie-image-samples/inception.jpeg'],
+            ['id' => 5, 'title' => 'Man Of Steel', 'releaseDate' => 2013, 'director' => 'Christopher Nolan', 'duration' => '143', 'image' => '/assets/images/movie-image-samples/man-of-steel.jpeg'],
+            ['id' => 6, 'title' => 'Dunkirk', 'releaseDate' => 2017, 'director' => 'Christopher Nolan', 'duration' => '106', 'image' => '/assets/images/movie-image-samples/dunkirk.jpeg'],
+        ];
     }
 }
